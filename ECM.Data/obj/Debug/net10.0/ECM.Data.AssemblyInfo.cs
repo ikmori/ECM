aa9310b9d990a6f877dbb908ae14ba28d756fd80
Helbert Garcia <helbert1001@gmail.com>
2026-02-09 10:11:25 -0400

    Agregando api

--- ECM.Data/obj/Debug/net10.0/ECM.Data.AssemblyInfo.cs
+++ ECM.Data/obj/Debug/net10.0/ECM.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECM.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db4ee97ea739043d695ff0f241319f43b12a077d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9e059459a5158f476f0f43d41787e8ed782b7d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECM.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECM.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
